77a866db9cdcd3b2ccfe0669ef78f1a0a17d9943
dhruv patel <d7511162@gmail.com>
2024-08-27 16:18:55 +0530

    query dev tools open

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <App />
+      <ReactQueryDevtools initialIsOpen={false} />
     </Provider>
   </QueryClientProvider>
 );
