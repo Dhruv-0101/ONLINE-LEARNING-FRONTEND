fb96656adb0e52efc7e56769cc1427d068606eaa
dhruv patel <d7511162@gmail.com>
2025-03-03 14:18:48 +0530

    success uri changed

--- src/components/payment/checkout.jsx
+++ src/components/payment/checkout.jsx
@@ -40,7 +40,7 @@ const CheckoutForm = () => {
             elements,
             clientSecret: paymentMutation.data?.clientSecret,
             confirmParams: {
-              return_url: "https://online-learning-frontend-seven.vercel.app/success",
+              return_url: "https://online-learning-frontend-seven.vercel.app//success",
             },
           });
           setErrorMessage(error?.message);
